177886055b7ac6cf78e8ec371e2130ec83fd3b61
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 00:15:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,7 +44,7 @@ class Owner
     self.pets[:dogs].each {|dog| dog.mood = 'happy'}
   end
 
-  def plays_with_cats
+  def play_with_cats
     self.pets[:cats].each {|cat| cat.mood = 'happy'}
   end
 
