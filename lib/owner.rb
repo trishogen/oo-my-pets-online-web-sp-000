be2ca6b7f3afd61168c534fdd12efd7298b9c260
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 00:07:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,9 @@
 class Owner
-  attr_reader :name, :species
+  attr_accessor :name
+  attr_reader :species
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize()
     @species = 'human'
     @@all << self
   end
